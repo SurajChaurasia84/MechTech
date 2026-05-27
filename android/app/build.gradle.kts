3b22577ad9da9bdac4f9f6a99c120570510dacc3
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-05-27 17:57:19 +0530

    chore: update Android build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mechtech"
+    namespace = "com.mechtech.mechanic.apps"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mechtech"
+        applicationId = "com.mechtech.mechanic.apps"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
