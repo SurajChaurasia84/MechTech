810f7e4b51e59604526289199addd3e05a685c58
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-05-27 22:49:53 +0530

    test: update widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,19 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_platform_interface/test.dart';
 import 'package:provider/provider.dart';
 import 'package:mechtech/main.dart';
 import 'package:mechtech/services/app_state.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  setupFirebaseCoreMocks();
+
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
+
   testWidgets('Login screen smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(
@@ -24,6 +26,6 @@ void main() {
     // Verify that login elements exist
     expect(find.text('MechTech'), findsOneWidget);
     expect(find.text('Premium Mechanic Service App'), findsOneWidget);
-    expect(find.text('Enter App'), findsOneWidget);
+    expect(find.text('Continue with Google'), findsOneWidget);
   });
 }
