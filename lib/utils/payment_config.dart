847ebc374264e4306d8bf774056a9778fc3e52f2
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-13 15:53:28 +0530

    feat(payment): add payment configuration utility

---
+++ lib/utils/payment_config.dart
@@ -0,0 +1,5 @@
+class PaymentConfig {
+  static const String razorpayKeyId = 'rzp_live_T0c78F2TcNcIwJ';
+  static const double commissionRate = 0.07; // 7% commission
+  static const String backendBaseUrl = 'https://vercel-ten-gray-35.vercel.app';
+}
